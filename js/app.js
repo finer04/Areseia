340641747a08dd575ae6abee4a50de1db06d8efa
finer04 <popkart7175@qq.com>
2018-05-20 15:27:46 +0800

    fix zoom size problem when user scroll
    Edge 滚动的时候，js 获得的
     background-size 过大，只好固定为
     2000px

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(function(){
     zoom = $('.banner').css('background-size')
     zoom = parseFloat(zoom)/100
-    size = zoom * $('.banner').width();
+    size = 2000;
     $(window).on('scroll', function(){
       fromTop = $(window).scrollTop();
       newSize = size - (fromTop/3);
@@ -22,7 +22,7 @@ $(function (){
     var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
     var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
 
-    if (isChrome || isSafari) {
+    if (isChrome || isSafari ) {
     } else {
         $('.banner').append('<div class="opaque"></div>');
         $(window).on('scroll', function(){
@@ -37,6 +37,7 @@ new WOW().init();
 })
 
 $(document).ready(function(){
+		$(".navbar").headroom();
 	init();
 });
 
@@ -49,5 +50,6 @@ $(function (){
 	$("table").addClass("table table-hover");
 	$("thead").addClass("thead-light");
 	$("#comments p").addClass("mb-1");
+	$(".navbar").headroom();
 })
 }  
